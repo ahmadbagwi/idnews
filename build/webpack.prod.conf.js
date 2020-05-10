b295c4b2e81b2f12dd6b4803d2421428008deb65
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2020-05-10 14:43:56 +0700

    add runtimeCaching url api

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -108,7 +108,28 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/berita-news\.herokuapp\.com/,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/newsapi\.org\/v2\/top-headlines?sources=bbc-news%2Cthe-verge/,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/code\.getmdl\.io\//,
+          handler: 'cacheFirst'
+        }]
     })
   ]
 })
