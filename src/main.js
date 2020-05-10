fb3bf8bbbde175cdd81bb1b2b192845bfd28723d
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2020-05-10 11:48:08 +0700

    import module, api

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
 
+const NewsAPI = require('newsapi')
+Vue.prototype.$newsapi = new NewsAPI('b19dc42ce86f4420b353e9fe419b64eb')
+Vue.prototype.$loading = true
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
