892639fdb8b448e2252cc31a8e4f72c2fa18c51b
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2020-05-10 11:48:28 +0700

    import components

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,26 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/international',
+      name: 'international',
+      component: () => import('@/components/International.vue')
+    },
+    {
+      path: '/sindo',
+      name: 'sindo',
+      component: () => import('@/components/Sindo.vue')
+    },
+    {
+      path: '/sindo/:url',
+      name: 'sindo-show',
+      component: () => import('@/components/SindoShow.vue')
+    },
+    {
+      path: '/covid',
+      name: 'covid',
+      component: () => import('@/components/Covid.vue')
     }
   ]
 })
